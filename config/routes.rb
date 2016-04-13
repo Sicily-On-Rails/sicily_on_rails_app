9fd9d949d3d44b2e91b508e2e4ecf0f8fa8d8dff
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2016-04-13 22:29:23 +0200

    Added namespace admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   #get 'home/index'
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  namespace :admin do
+    root to: 'dashboard#index'
+  end
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 end
